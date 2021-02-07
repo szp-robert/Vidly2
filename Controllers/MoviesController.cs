df900e1e9097eea975e6d516c4eecb55a69b4f58
Robert Zalewski <rzalewski123@gmail.com>
2021-02-07 14:58:17 +0100

    Section 4 - finished

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -21,6 +21,37 @@ namespace Vidly2.Controllers
         {
             _context.Dispose();
         }
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if(movie.Id==0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);              
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index","Movies");
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+            return View("MovieForm", viewModel);
+        }
 
         public ViewResult Index()
         {
@@ -57,6 +88,20 @@ namespace Vidly2.Controllers
                 return HttpNotFound();
 
             return View(movie);
+        }
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return RedirectToAction("New","Movies");
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm", viewModel);
         }
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,23 @@ namespace Vidly2.Models
     public class Movie
     {
         public int Id { get; set; }
+               
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
         public Genre Genre { get; set; }
+
+        [Required]
         public byte GenreId { get; set; }
+                
+        [Display(Name = "Release Date")]
         public Nullable<DateTime> ReleaseDate { get; set; }
+                
+        [Display(Name="Date Added")]
         public DateTime DateAdded { get; set; }
+               
+        [Display(Name = "Number in stock")]
         public int NumberInStock { get; set; }
 
     }
